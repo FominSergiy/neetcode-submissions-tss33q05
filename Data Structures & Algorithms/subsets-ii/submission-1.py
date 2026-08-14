016cce46452c0509cc04c1bcaeb528dd624f0aec
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-08-14 17:29:10 -0400

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # 2 ways 1, skip dups, 2nd count binary choices and pass forward
+
+        # option 1, build as we go
+        # def backtrack(i: int, cur: List[int]):
+        #     ans.append(cur[:])
+
+        #     if i == len(nums):
+        #         return
+
+        #     for j in range(i, len(nums)):
+        #         if j > i and nums[j] == nums[j - 1]:
+        #             continue
+                
+        #         cur.append(nums[j])
+        #         backtrack(j + 1, cur)
+        #         cur.pop()
+
+        # option 2 - build on choices
+        def backtrack(i: int, cur: list[int]):
+            if i == len(nums):
+                ans.append(cur[:])
+                return
+            
+            # choice 1 - pick this number
+            cur.append(nums[i])
+            backtrack(i + 1, cur)
+            cur.pop()
+
+            # skip dups in sorted input
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            
+            # choice 2 - skip this num and skip dups
+            backtrack(i + 1, cur)        
+
+        ans = []
+        nums.sort()
+        backtrack(0, [])
+        return ans
