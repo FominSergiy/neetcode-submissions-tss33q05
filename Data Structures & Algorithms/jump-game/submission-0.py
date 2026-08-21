254d0ef37516ea697897ab3c9d876a6f1b1b30a0
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-08-21 15:57:06 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        # go backward from end
+        # at each i we check against target
+            # if we can reach target, we update target to be this i
+                # this means we can reach target from this i
+                # find earlist i that can reach this target
+        
+        # index we need to reach
+        target = len(nums) - 1
+
+        for i in range(len(nums) -2, -1, -1):
+            if i + nums[i] >= target:
+                target = i
+
+        return True if target == 0 else False
+        
