672c121fda0fce01a687f68cfc9298f07a07aaaa
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-08-21 14:48:49 -0400

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        cols = set()
+        diag = set() # row - col
+        diag_n = set() # row + col
+        ans = []
+
+        # pass row, iterate on column
+        def backtrack(row: int):
+            if row >= n:
+                ans.append(["".join(row) for row in board[:]])
+                return
+            
+            for col in range(n):
+                # check if we can place queen
+                if (
+                    not col in cols and
+                    not row - col in diag and
+                    not row + col in diag_n
+                ):
+                    cols.add(col)
+                    diag.add(row - col)
+                    diag_n.add(row + col)
+                    board[row][col] = 'Q'
+
+                    backtrack(row + 1)
+
+                    cols.remove(col)
+                    diag.remove(row - col)
+                    diag_n.remove(row + col)
+                    board[row][col] = '.'
+        
+        backtrack(0)
+        return ans
