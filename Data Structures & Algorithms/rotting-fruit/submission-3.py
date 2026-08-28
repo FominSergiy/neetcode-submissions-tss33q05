ef1c868e3a9bbc09c89ad1964ea1804ce5122ff9
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-08-28 18:32:30 -0400

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        minutes = 0
+        total_fresh = 0
+
+        def valid(row: int, col: int) -> bool:
+            return 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == 1
+        
+        def bfs(q: int):
+            nonlocal minutes
+            nonlocal total_fresh
+
+            while total_fresh > 0 and q:
+                # print(q)
+                for _ in range(len(q)):
+                    r, c = q.popleft()
+                    
+                    for dr, dc in moves:
+                        nr, nc = dr + r, dc + c
+                        if valid(nr, nc):
+                            total_fresh -= 1
+                            grid[nr][nc] = 2
+                            q.append((nr, nc))
+                minutes += 1
+
+        queue = deque([])
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 2:
+                    queue.append([row, col])
+                if grid[row][col] == 1:
+                    total_fresh += 1
+    
+        bfs(queue)
+        return minutes if total_fresh == 0 else -1
+                    
