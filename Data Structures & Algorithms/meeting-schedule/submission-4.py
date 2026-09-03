7af944e8c76f4493238424d0174b9c931a07ca1e
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-03 14:02:40 -0400

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+from collections import defaultdict
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        # sorting and updating latest intervas
+        intervals.sort(key=lambda interval: interval.start)
+        last_e = intervals[0].end
+
+        for interval in intervals[1:]:
+            if last_e > interval.start:
+                return False
+            else:
+                last_e = interval.end
+        return True
+
+
+        # single - line sweep
+
