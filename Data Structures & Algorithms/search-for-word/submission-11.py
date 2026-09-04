ecbf90fefa5d61b801232c113e8ed44f68d8aec8
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-04 13:55:58 -0400

    Add: search-for-word - submission-11

---
+++ Data Structures & Algorithms/search-for-word/submission-11.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # only move into valid
+        # modify in place
+        # dfs into 4 nearby positions
+        # run only on start of character
+        ROWS, COLS = len(board), len(board[0])
+
+        def valid(row: int, col: int) -> bool:
+            return 0 <= row < ROWS and 0 <= col < COLS
+        
+        def dfs(row: int, col: int, i: int) -> bool:
+            if i == len(word):
+                return True
+            
+            if not valid(row, col) or board[row][col] != word[i]:
+                return False
+            
+            board[row][col] = "#"
+            ans = (
+                dfs(row + 1, col, i + 1) or
+                dfs(row - 1, col, i + 1) or
+                dfs(row, col + 1, i + 1) or
+                dfs(row, col - 1, i + 1)
+            )
+            board[row][col] = word[i]
+            return ans
+        
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == word[0]:
+                    if dfs(row, col, 0):
+                        return True
+        return False        
