de134ddc951e4f844383cdf12d34121fce64b3d4
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-07 12:02:16 -0400

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # valid tree is one that
+        # 1. has no cycles
+        # 2. all nodes are reachable
+
+        # since we have undirected edges
+        # we make a by-directional list and dfs from 0 node
+        # we we encounter visited node -> cycle
+        # otherwise keep traversing
+        if len(edges) > n:
+            return False
+        
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        def dfs(node: int, par: int):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+            for nei in adj[node]:
+                # in cycle detection need to skip parent, always
+                if nei == par:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+        
+        visited = set()
+        # there should not only not be a cycle
+        # BUT all nodes have to be reachable
+        return dfs(0, -1) and len(visited) == n
+
+
+
+
