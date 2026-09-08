105b7611cf42b30f1f61f2cfcf28659d65aef84c
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-08 12:07:07 -0400

    Add: foreign-dictionary - submission-6

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-6.py
@@ -0,0 +1,45 @@
+from collections import deque
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        # use topological sort to sort out all indegrees
+        # cycles would prove if indegrees are left
+        adj = { c: set() for w in words for c in w }
+        indegree = { c: 0 for c in adj }
+
+        # build adj list based on the rules of lexico sort
+        n = len(words)
+        for i in range(n - 1):
+            w1, w2 = words[i], words[i + 1]
+            min_len = min(len(w1), len(w2))
+
+            # exit early if w1 is longer prefix of w2
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
+                return ""
+            
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break # we only need to add 1st occurance of such char
+        
+        queue = deque()
+        for char in indegree:
+            if indegree[char] == 0:
+                queue.append(char)
+
+        ans = []
+        while queue:
+            char = queue.popleft()
+            ans.append(char)
+
+            for nei in adj[char]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        
+        return "".join(ans) if len(ans) == len(indegree) else ""
+
+                
+
+        
