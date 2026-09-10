f11dea6073c90bd8b3da72cc90c062cbb9ff0734
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-10 14:49:59 -0400

    Add: swim-in-rising-water - submission-8

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-8.py
@@ -0,0 +1,27 @@
+from heapq import *
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+
+        def valid(row: int, col: int) -> bool:
+            return 0 <= row < N and 0 <= col < N
+        
+        heap = [(grid[0][0], 0, 0)] # (value t, r, c)
+
+        while heap:
+            t, r, c = heapq.heappop(heap)
+
+            if r == N - 1 and c == N - 1:
+                return t
+            
+            for dr, dc in moves:
+                nr, nc = dr + r, dc + c
+                if valid(nr, nc) and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    heapq.heappush(heap, (max(t, grid[nr][nc]), nr, nc))
+        
+         
+
+        
