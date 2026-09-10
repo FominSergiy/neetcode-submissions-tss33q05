bb9b9bb09a4b1b5a77178537170d93d9b932dde5
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-10 13:33:53 -0400

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ansIdx, ansLen = 0, 0
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                l, r = i, j
+                if s[l] == s[r] and (
+                    r - l <= 2 or
+                    dp[l + 1][r - 1]
+                ):
+                    dp[l][r] = True
+                    # check if current resLen is <
+                    if ansLen < r - l + 1:
+                        ansIdx = l
+                        ansLen = r - l + 1
+        
+        return s[ansIdx: ansIdx + ansLen]
+        
