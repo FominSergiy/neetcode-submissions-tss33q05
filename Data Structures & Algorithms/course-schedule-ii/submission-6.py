d0245eb62fd96335c79754874788b0638fec24a9
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-10 14:03:58 -0400

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # return len
+        # EDGES HAVE DIRECTION!
+        # indegree count but make it pre-req -> course
+        # to build correct output
+        n = numCourses
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+
+        for crs, pre in prerequisites:
+            indegree[crs] += 1
+            adj[pre].append(crs)
+        
+        queue = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+            
+        output = []
+        while queue:
+            node = queue.popleft()
+            output.append(node)
+
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        
+        return output if len(output) == n else []
