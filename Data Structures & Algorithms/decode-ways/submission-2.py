a616d46e75b4427e6321465cf51daf7896be8fcb
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-11 09:25:59 -0400

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        memo = {}
+
+        def dp(i: int) -> int:
+            if i == n:
+                return 1
+            
+            if s[i] == "0":
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            
+            # otherwise its a valid choice, step
+            res = dp(i + 1)
+            # try choice with next char
+            if i < n - 1:
+                if (
+                    s[i] == '1' or
+                    s[i] == '2' and s[i + 1] < "7"
+                ):
+                    res += dp(i + 2)
+
+            memo[i] = res
+            return memo[i]
+
+
+        return dp(0)
