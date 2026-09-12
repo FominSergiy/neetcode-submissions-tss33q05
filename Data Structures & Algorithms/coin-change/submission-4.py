6d78fe977751eeac87f39dd986f37135a490c5a1
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-12 13:07:17 -0400

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        # TOP-DOWN
+        memo = {}
+        def dp(amt: int):
+            if amt == 0:
+                return 0
+            
+            if amt in memo:
+                return memo[amt]
+            
+            res = float('inf')
+            for c in coins:
+                if amt - c >= 0:
+                    res = min(res, dp(amt - c) + 1)
+            memo[amt] = res
+            return memo[amt]
+
+            
+        ans = dp(amount)
+        return ans if ans != float('inf') else -1
+        #BOTTOM-UP
+        # dp = [amount + 1] * (amount + 1)
+        # dp[0] = 0 # we reached starting point
+        # for a in range(1, amount + 1):
+        #     for c in coins:
+        #         if a - c >= 0:
+        #             # min of current amount or taking current coin and adding one
+        #             dp[a] = min(dp[a], dp[a - c] + 1)
+        
+        # # only return amount if we could actually get the amount given coins we have
+        # return dp[amount] if dp[amount] != amount + 1 else -1
+        
