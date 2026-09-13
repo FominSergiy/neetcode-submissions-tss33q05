0295e35262a4fce43a91e644e0e3b4c32e77b761
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-13 15:04:46 -0400

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        memo = [-1] * n
+
+        def dp(i: int):
+            if memo[i] != -1:
+                return memo[i]
+            
+
+            LIS = 1 # sub-seq at any idx is 1
+            # try to build logest sub-seq at this idx
+            for j in range(i + 1, n):
+                if nums[i] < nums[j]:
+                    LIS = max(LIS, dp(j) + 1)
+            
+            memo[i] = LIS
+            return memo[i]
+        
+        # need to consider ALL sub-seq starting at any i
+        return max([dp(i) for i in range(n)])
