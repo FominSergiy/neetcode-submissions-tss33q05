886af6325d232137e106a1eabadddb1bdb07a2a8
Sergiy Fomin <65659492+FominSergiy@users.noreply.github.com>
2026-09-15 15:40:55 -0400

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # TOP DOWN
+        # word_set = set(wordDict)
+        # n = len(s)
+        # if n == 1:
+        #     return s in word_set
+        
+        # # build top-down dp with start: int as starting idx, use 2 pointer to pattern match words from longer str
+        # memo = {}
+        # def dp(start: int) -> bool:
+        #     if start == n:
+        #         return True # reached the end
+            
+        #     if start in memo:
+        #         return memo[start]
+        #     can_split = False
+        #     for end in range(start, n):
+        #         word = s[start: end + 1]
+        #         if word in word_set: # valid candidate - try this word
+        #             can_split |= dp(end + 1)
+            
+        #     memo[start] = can_split
+        #     return memo[start]
+        
+        # return dp(0)
+
+        # BOTTOM UP
+        n = len(s)
+        # n + 1 needed because this builds an exclusive size up to n + 1 -> meaning n is available
+        # otherwise if size n -> then idx n will not be available
+        dp = [False] * (n + 1)
+        dp[n] = True
+
+        for i in range(n - 1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= n and s[i: i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                
+                if dp[i]:
+                    break
+        
+        return dp[0]
+        
